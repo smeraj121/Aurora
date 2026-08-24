63b6b08884268e8d144e693449cfc13444073250
Saba Meraj <saba.meraj@nagarro.com>
2026-08-25 00:17:27 +0530

    ssl for production

--- Aurora/backend/db.js
+++ Aurora/backend/db.js
@@ -8,6 +8,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = {
